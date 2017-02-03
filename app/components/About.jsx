25ac2ac11d08757bfa8994088d5d6f0e112f613f
Pratik <pratzdj@gmail.com>
2017-02-03 22:46:54 +0530

    added content to about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,8 +5,15 @@ var React = require('react');
 var About = (props) => {
 	return (
 			<div>
-				<h3>About</h3>
-				<p>Welcome to the about page!</p>
+				<h3 class="text-center">About</h3>
+				<p>This is a weather application that I made to learn and understand React</p>
+				<p><a href='https://facebook.github.io/react' target='_blank'>React</a> is a front-end library made by Facebook for creating
+				user-interfaces.
+				</p>
+				<p>I have also used <a href='http://openweathermap.org' target='_blank'>openweathermap</a> to search
+				   for weather data by city name
+				</p>
+
 			</div>
 		)
 };
